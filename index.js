bab3c760d62ec9f9efd9965f67283b30ab2745e3
benjamingeets <geets@tuta.io>
2022-05-23 21:47:03 +0200

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const data = await Deno.readTextFile("./data.json")
+const postcodes = await JSON.parse(data)
+
+const router = new Router();
+router
+  .get("/:code", (context) => {
+    const { code } = context.params;
+    const cities  = postcodes.filter(city => city.postcode === code)
+    context.response.body = cities;
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 80 });
